66417cce69905778a0d9a5bb3774b0f575d96dca
CompMage <compmage@gmail.com>
2019-02-27 14:49:09 -0500

    Basic

---
+++ Bitlocker.ps1
@@ -0,0 +1,3 @@
+﻿Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes256 -TPMProtector -SkipHardwareTest
+Add-BitLockerKeyProtector -MountPoint "C:" -RecoveryKeyPath "D:\" -RecoveryKeyProtector
+(Get-BitLockerVolume -MountPoint C).KeyProtector.recoverypassword > D:\bitlockerkey.txt
---
+++ Thumb-Drive.ps1
@@ -0,0 +1,9 @@
+﻿Get-Disk | Where-Object -FilterScript {$_.Bustype -Eq "USB"}
+#$DDis = Read-Host -Prompt "Select Disk to use"
+$DDis = 2
+Clear-Disk -Number $DDis -Confirm:$false -RemoveData
+#New-Partition -DiskNumber $DDis -Size 30GB -AssignDriveLetter | Format-Volume -FileSystem Fat32
+New-Partition -DiskNumber $DDis -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem Fat32
+Get-Partition -DiskNumber $DDis | Set-Partition -NewDriveLetter P
+ 
+Copy-Item "D:\WinPE_amd64\WinPE_w_tools\*" -Destination P:\ -Recurse -Force -Verbose
