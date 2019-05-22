d88559edd52c625fccd12fe368988c88b71086a8
CompMage <compmage@gmail.com>
2019-05-22 13:49:07 -0400

    Update Hyper-V_Gen2.ps1

--- Hyper-V_Gen2.ps1
+++ Hyper-V_Gen2.ps1
@@ -1,15 +1,12 @@
-﻿# This script configures the Hyper-V machines used for the 50331 Course.
-# PowerShell 3.0 and Windows Server 2012 or Windows 8 Pro are required to perform this setup.
-# The C:\ Drive should have at least 200GB of free space available.
-# All the files on the 50331 Student CD should be copied to C:\Labfiles before performing this setup.
-
-# Variables
-$CLI1 = "SILCIO-X-CMHER4"		# Name of VM running Client Operating System
-$CRAM = 8GB				                # RAM assigned to Client Operating System
-$CLI1VHD = 60GB				                # Size of Hard-Drive for Client Operating System
-$VMLOC = "D:\Working\VM"			        # Location of the VM and VHDX files
-$NetworkSwitch1 = "PrivateSwitch1"	# Name of the Network Switch
-$CMBOOT = "D:\Working\ISO\HQ2_Exp_CMBOOT.iso"	# PE SCCM Booter
+﻿# Variables
+$CLI1 = "2016_Test"		           # Name of VM running Client Operating System
+$VMProc = 2
+$CRAM = 8GB				                       # RAM assigned to Client Operating System
+$MRAM = 4GB                                    # Min Memory Size
+$CLI1VHD = 60GB				                   # Size of Hard-Drive for Client Operating System
+$VMLOC = "D:\Working\VM"			           # Location of the VM and VHDX files
+$NetworkSwitch1 = "HQ - Virtual Switch"	       # Name of the Network Switch
+$CMBOOT = "D:\Working\ISO\14393.0.161119-1705.RS1_REFRESH_SERVERHYPERCORE_OEM_X64FRE_EN-US.ISO"	# Installer / Booter
 
 
 # Create VM Folder and Network Switch
@@ -17,12 +14,20 @@ MD $VMLOC -ErrorAction SilentlyContinue
 $TestSwitch = Get-VMSwitch -Name $NetworkSwitch1 -ErrorAction SilentlyContinue; if ($TestSwitch.Count -EQ 0){New-VMSwitch -Name $NetworkSwitch1 -SwitchType Private}
 
 # Create Virtual Machines
-New-VM -Name $CLI1 -Path $VMLOC -MemoryStartupBytes $CRAM -NewVHDPath $VMLOC\$CLI1.vhdx -NewVHDSizeBytes $CLI1VHD -SwitchName $NetworkSwitch1
+New-VM -Name $CLI1 -Path $VMLOC -MemoryStartupBytes $CRAM -SwitchName $NetworkSwitch1 -NewVHDPath $VMLOC\$CLI1\Drives\$CLI1.vhdx -NewVHDSizeBytes $CLI1VHD -Generation 2
 
-# Configure Virtual Machines
+Set-VMProcessor -VMName $CLI1 -Count 2
+
+Set-VM -VMName $CLI1 -MemoryStartupBytes $CRAM -DynamicMemory -MemoryMinimumBytes $MRAM -MemoryMaximumBytes $CRAM
+
+Set-VMMemory -VMName $CLI1 -Buffer 10 -Priority 50
+
+# DVD Drive Settings
+Add-VMDvdDrive -VMName $CLI1 -ControllerNumber 0 -ControllerLocation 5
+
 Set-VMDvdDrive -VMName $CLI1 -Path $CMBOOT
 
-# Settings
-Set-VMProcessor -VMName $CLT1 -Count 2 -Reserve 10 -Maximum 85
+# Enable Resouse Monitoring. Disable if not used in environment.
+Enable-VMResourceMetering -VMName $CLI1
 
-Start-VM $CLI1
+#Start-VM $CLI1
