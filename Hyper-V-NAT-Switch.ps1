a8d7c48a5571c9c195f095cdd5cc8aad4caf6cae
Christopher Hermance <42744794+CompMage@users.noreply.github.com>
2023-03-13 12:59:43 -0400

    Add files via upload

---
+++ Hyper-V-NAT-Switch.ps1
@@ -0,0 +1,18 @@
+﻿#Vegetables
+$SwitchName = "CMLab_Switch"
+$NetworName = "CMLab_External"
+
+#Builds and Internal Switch
+New-VMSwitch -SwitchName $SwitchName -SwitchType Internal
+
+#Get the Network adaptrer information for the next step based on the last.
+$IndesID = Get-NetAdapter -Name "vEthernet ($SwitchName)" 
+
+#Assigns an IP address and subnetmask to thr Virtual Switch Network Adapter
+New-NetIPAddress -IPAddress 192.168.10.1 -PrefixLength 24 -InterfaceIndex $IndesID.ifIndex
+
+#Builds the Actual Nat 
+New-NetNat -Name $NetworName -InternalIPInterfaceAddressPrefix 192.168.10.0/24
+
+#Connects all VM to NAT Network
+Get-VM | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName $SwitchName
--- Hyper-V_Gen2.ps1
+++ Hyper-V_Gen2.ps1
@@ -1,33 +1,42 @@
 ﻿# Variables
-$CLI1 = "2016_Test"		           # Name of VM running Client Operating System
+$CRAM = 8GB
+$LRAM = 2GB
+$MRAM = 4GB
+$VMName = "1809_Test_3"		           # Name of VM running Client Operating System
 $VMProc = 2
-$CRAM = 8GB				                       # RAM assigned to Client Operating System
-$MRAM = 4GB                                    # Min Memory Size
-$CLI1VHD = 60GB				                   # Size of Hard-Drive for Client Operating System
-$VMLOC = "D:\Working\VM"			           # Location of the VM and VHDX files
+$VMRam = 8GB				                       # RAM assigned to Client Operating System
+$VMLRam = 2GB                                    # Min Memory Size
+$VMSystemVHD = 60GB				                   # Size of Hard-Drive for Client Operating System
+$VMLoc = "D:\Working\VM"			           # Location of the VM and VHDX files
+
 $NetworkSwitch1 = "HQ - Virtual Switch"	       # Name of the Network Switch
-$CMBOOT = "D:\Working\ISO\14393.0.161119-1705.RS1_REFRESH_SERVERHYPERCORE_OEM_X64FRE_EN-US.ISO"	# Installer / Booter
+$ISOLoc= "D:\Working\ISO\CMBoot_19.iso"	# Installer / Booter
 
-
-# Create VM Folder and Network Switch
-MD $VMLOC -ErrorAction SilentlyContinue
-$TestSwitch = Get-VMSwitch -Name $NetworkSwitch1 -ErrorAction SilentlyContinue; if ($TestSwitch.Count -EQ 0){New-VMSwitch -Name $NetworkSwitch1 -SwitchType Private}
+Remove-VM $VMName -Confirm:$false -ErrorAction SilentlyContinue
+Remove-Item $VMLoc\$VMName -Recurse -Force -ErrorAction SilentlyContinue
 
 # Create Virtual Machines
-New-VM -Name $CLI1 -Path $VMLOC -MemoryStartupBytes $CRAM -SwitchName $NetworkSwitch1 -NewVHDPath $VMLOC\$CLI1\Drives\$CLI1.vhdx -NewVHDSizeBytes $CLI1VHD -Generation 2
+New-VM -Name $VMName -Path $VMLoc -SwitchName $NetworkSwitch1 -MemoryStartupBytes $LRAM -NewVHDPath $VMLoc\$VMName\Drives\System_$VMName.vhdx -NewVHDSizeBytes $VMSystemVHD -Generation 2
 
-Set-VMProcessor -VMName $CLI1 -Count 2
+Set-VM -VMName $VMName -MemoryStartupBytes $CRAM -MemoryMinimumBytes $MRAM -MemoryMaximumBytes $CRAM -ProcessorCount $VMProc
 
-Set-VM -VMName $CLI1 -MemoryStartupBytes $CRAM -DynamicMemory -MemoryMinimumBytes $MRAM -MemoryMaximumBytes $CRAM
+Set-VMMemory -VMName $VMName -Buffer 10 -Priority 50
 
-Set-VMMemory -VMName $CLI1 -Buffer 10 -Priority 50
+Set-VMFirmware -VMName $VMName -EnableSecureBoot On -SecureBootTemplate MicrosoftWindows
+
+# Extra Drives
+New-VHD -Path $VMLoc\$VMName\Drives\Data_$VMName.vhdx -SizeBytes 100GB -Dynamic
+
+Add-VMHardDiskDrive -VMName $VMName -Path $VMLoc\$VMName\Drives\Data_$VMName.vhdx -ControllerType SCSI -ControllerNumber 0 -ControllerLocation 2
 
 # DVD Drive Settings
-Add-VMDvdDrive -VMName $CLI1 -ControllerNumber 0 -ControllerLocation 5
+Add-VMDvdDrive -VMName $VMName -ControllerNumber 0 -ControllerLocation 5
 
-Set-VMDvdDrive -VMName $CLI1 -Path $CMBOOT
+Set-VMDvdDrive -VMName $VMName -Path $ISOLoc
 
 # Enable Resouse Monitoring. Disable if not used in environment.
-Enable-VMResourceMetering -VMName $CLI1
+Enable-VMResourceMetering -VMName $VMName
 
-#Start-VM $CLI1
+Set-VMHost -EnableEnhancedSessionMode $true
+
+Start-VM $VMName
