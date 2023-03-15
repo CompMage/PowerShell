45891a4013c491a0f353ecccf845a5bba311e3d8
Christopher Hermance <42744794+CompMage@users.noreply.github.com>
2023-03-15 00:30:31 -0400

    Update SQL Ports.ps1

--- SQL Ports.ps1
+++ SQL Ports.ps1
@@ -1,6 +1,6 @@
-﻿Write-host ========= SQL Server Ports ===================
+﻿Write-host "========= SQL Server Ports ==================="
  
-Write-host Enabling SQLServer default instance port 1433
+Write-host "Enabling SQLServer default instance port 1433"
  
 #netsh firewall set portopening TCP 1433 "SQLServer"
  
@@ -8,7 +8,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 1433" -Group "SQL Serve
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 1433" -Group "SQL Server Ports" –Direction outbound –LocalPort 1433 -Protocol TCP -Action Allow
  
-Write-host Enabling Dedicated Admin Connection port 1434
+Write-host "Enabling Dedicated Admin Connection port 1434"
  
 #netsh firewall set portopening TCP 1434 "SQL Admin Connection"
  
@@ -16,7 +16,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 1434" -Group "SQL Serve
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 1434" -Group "SQL Server Ports" -Direction outbound –LocalPort 1434 -Protocol TCP -Action Allow
  
-Write-host Enabling conventional SQL Server Service Broker port 4022
+Write-host "Enabling conventional SQL Server Service Broker port 4022"
  
 #netsh firewall set portopening TCP 4022 "SQL Service Broker"
  
@@ -24,7 +24,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 4022" -Group "SQL Serve
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 4022" -Group "SQL Server Ports" -Direction outbound –LocalPort 4022 -Protocol TCP -Action Allow
  
-Write-host Enabling Transact-SQL Debugger/RPC port 135
+Write-host "Enabling Transact-SQL Debugger/RPC port 135"
  
 #netsh firewall set portopening TCP 135 "SQL Debugger/RPC"
  
@@ -32,7 +32,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 135" -Group "SQL Server
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 135" -Group "SQL Server Ports" -Direction outbound –LocalPort 135 -Protocol TCP -Action Allow
 
-Write-host Enabling port for SQL Server Browser Service's 'Browse
+Write-host "Enabling port for SQL Server Browser Service's 'Browse"
 
 #netsh firewall set portopening UDP 1434 "SQL Browser"
  
@@ -40,9 +40,9 @@ New-NetFirewallRule -DisplayName "Allow inbound UDP Port 1434" -Group "SQL Serve
  
 New-NetFirewallRule -DisplayName "Allow outbound UDP Port 1434" -Group "SQL Server Ports" -Direction outbound –LocalPort 1434 -Protocol UDP -Action Allow
  
-Write-host ========= Analysis Services Ports ==============
+Write-host "========= Analysis Services Ports =============="
  
-Write-host Enabling SSAS Default Instance port 2383
+Write-host "Enabling SSAS Default Instance port 2383"
  
 #netsh firewall set portopening TCP 2383 "Analysis Services"
  
@@ -50,7 +50,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 2383" -Group "SQL Analy
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 2383" -Group "SQL Analysis Services Ports" -Direction outbound –LocalPort 2383 -Protocol TCP -Action Allow
  
-Write-host Enabling SQL Server Browser Service port 2382
+Write-host "Enabling SQL Server Browser Service port 2382"
  
 #netsh firewall set portopening TCP 2382 "SQL Browser"
  
@@ -58,9 +58,9 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 2382" -Group "SQL Analy
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 2382" -Group "SQL Analysis Services Ports" -Direction outbound –LocalPort 2382 -Protocol TCP -Action Allow
  
-Write-host ========= Misc Applications ==============
+Write-host "========= Misc Applications =============="
  
-Write-host Enabling HTTP port 80
+Write-host "Enabling HTTP port 80"
  
 #netsh firewall set portopening TCP 80 "HTTP"
  
@@ -68,7 +68,7 @@ New-NetFirewallRule -DisplayName "Allow inbound TCP Port 80" -Direction inbound 
  
 New-NetFirewallRule -DisplayName "Allow outbound TCP Port 80" -Direction outbound –LocalPort 80 -Protocol TCP -Action Allow
  
-Write-host Enabling SSL port 443
+Write-host "Enabling SSL port 443"
  
 #netsh firewall set portopening TCP 443 "SSL"
  
